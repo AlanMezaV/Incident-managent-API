92b696f0bb4ee8cbbe7289ab0f2655634ae49500
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-12-18 23:23:11 -0700

    Added deploy url to cors

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://incident-deploy-97ti.vercel.app',
     credentials: true
 };
 app.use(cors(corsOptions));
