152f8eb23b24b4f5a0a8d537fe8b758e786d7f56
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-12-18 18:13:24 -0700

    Changed cors options twice

--- src/app.ts
+++ src/app.ts
@@ -22,10 +22,11 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 const corsOptions = {
-    origin: 'http://incident-managent-api-production.up.railway.app',
+    origin: ['http://localhost:5173', 'http://incident-managent-api-production.up.railway.app'],
     credentials: true
 };
 app.use(cors(corsOptions));
+
 app.use(cookieParser());
 
 // Routes
